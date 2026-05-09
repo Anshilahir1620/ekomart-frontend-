2b8713998ac34add50c1634fcc15e74a86cf4655
Chotara Anshil <anshilchotara@gmail.com>
2026-05-09 23:39:21 +0530

    Delete Backend directory

--- Backend/app/models/index.py
+++
@@ -1,6 +0,0 @@
-from models.users import User
-from models.subcategories import SubCategory
-from models.banners import Banner
-from models.brands import Brand
-from models.products import Product
-from models.roles import Role
--- Backend/app/models/roles.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String, TIMESTAMP
-from app.database import Base
-
-class Role(Base):
-    __tablename__ = "roles"
-
-    id = Column(Integer, primary_key=True)
-    role_name = Column(String(255))
