9ce10ac49145316a39839c4420e563e8f779cc90
Anshil Chotara <anshilchotara@gmail.com>
2026-05-11 23:02:38 +0530

    Fix checkout suspense issue

--- app/Chekout/page.tsx
+++ app/Chekout/page.tsx
@@ -1,9 +1,15 @@
 'use client';
 import React from 'react';
+import { Suspense } from 'react'
+
 import CheckoutPage from "@/app/Chekout/ChekoutPage"
 
 export default function ShopPage() {
-  return <CheckoutPage/>;
+    return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CheckoutPage />
+    </Suspense>
+  )
 
 
 }
